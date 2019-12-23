e3231c6e621250895c5860b2bfff956a33f67dad
AlexanderSanes <poletaevaa99@mail.ru>
2019-12-23 16:50:25 +0300

    comments fix

--- project/include/eigenvalue.h
+++ project/include/eigenvalue.h
@@ -9,18 +9,18 @@
 
 int fil = 1;
 
-TYPE 	*obrhod(TYPE **a1, TYPE *y1, TYPE *x, int n);     							//обратный ход метода Гаусса  
-TYPE 	*gauss(TYPE **a, TYPE *b, int n);											//прямой ход метода Гаусса
-int 	qrmeth(TYPE **a, TYPE **Q, TYPE **R, TYPE **T, int n);						//QR-разложение
-// TYPE 	**inverse_matrix(TYPE **a1, TYPE **Q, TYPE **R, TYPE **T, TYPE *b1, int m); //вычисление обратной матрицы
+TYPE 	*obrhod(TYPE **a1, TYPE *y1, TYPE *x, int n);     							//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ  
+TYPE 	*gauss(TYPE **a, TYPE *b, int n);											//пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+int 	qrmeth(TYPE **a, TYPE **Q, TYPE **R, TYPE **T, int n);						//QR-пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+// TYPE 	**inverse_matrix(TYPE **a1, TYPE **Q, TYPE **R, TYPE **T, TYPE *b1, int m); //пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 TYPE 	**hessenberg(TYPE **a1, int m);
-TYPE 	*EigenValues(TYPE **H1, int n);												//поиск собственных значений
-TYPE 	**EigenVectors(TYPE **a, TYPE *lambda, int n);								//поиск собвтвенных векторов
-TYPE 	**shift_minus(TYPE **a1, TYPE d1, int m);									//сдвиг диагонального элемента матрицы на -d
-TYPE 	**shift_plus(TYPE **a1, TYPE d1, int m);									//сдвиг диагонального элемента матрицы на +d
-TYPE 	EuclidMult(TYPE *b1, TYPE *b2, int m);										//Евклидово скалярное произведение
-TYPE 	**det_gauss(TYPE **a1, int m);												//вычисление определителя методом Гаусса
-TYPE 	trace(TYPE **a1, int m);													//след матрицы
-void 	chek(TYPE **a1, TYPE **H1, TYPE *lambd, TYPE **x1, int m);					//проверка собственных значений и собственных чисел
+TYPE 	*EigenValues(TYPE **H1, int n);												//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TYPE 	**EigenVectors(TYPE **a, TYPE *lambda, int n);								//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TYPE 	**shift_minus(TYPE **a1, TYPE d1, int m);									//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ -d
+TYPE 	**shift_plus(TYPE **a1, TYPE d1, int m);									//пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ +d
+TYPE 	EuclidMult(TYPE *b1, TYPE *b2, int m);										//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+TYPE 	**det_gauss(TYPE **a1, int m);												//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ
+TYPE 	trace(TYPE **a1, int m);													//пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+void 	chek(TYPE **a1, TYPE **H1, TYPE *lambd, TYPE **x1, int m);					//пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
 TYPE 	RayleighRatio(TYPE **a, TYPE **x1, TYPE *lambdaR, int m);
 
